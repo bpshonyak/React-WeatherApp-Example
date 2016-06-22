5905e8b7a038e8d51f27bac38e19b2516932ee6e
Bogdan Pshonyak <bpshonyak@live.com>
2016-06-22 05:38:40 -0700

    basic fetch weather action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,14 @@
 const API_KEY = "7f50becdcf7259b8db0f772e1ff2b6c3";
+const BASE_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city) {
+
+  const REQUEST_URL = `${BASE_URL}&q=${city},us`;
+
+  return {
+    type: FETCH_WEATHER,
+    payload: ''
+  }
+}
